3ed7c02c43df5dc636d284e8e97415461504f92e
Airon <maksim285@mail.ru>
2023-02-22 22:34:08 +0700

    changes after the remark

--- conftest.py
+++ conftest.py
@@ -2,12 +2,17 @@ from selene.support.shared import browser
 import pytest
 
 
-@pytest.fixture()
+@pytest.fixture(scope='session')
 def open_browser():
     browser.config.window_height = 900
     browser.config.window_width = 900
     browser.open('https://google.com')
 
+
+@pytest.fixture(scope='function')
+def clear_search():
+    yield
+    browser.element('[name="q"]').clear()
 
 
 
--- test_web.py
+++ test_web.py
@@ -2,11 +2,11 @@ from selene import browser
 from selene import be, have
 
 
-def test_open_selene(open_browser):
+def test_open_selene(open_browser, clear_search):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     assert browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
-def test_open_error(open_browser):
+def test_open_error(open_browser, clear_search):
     browser.element('[name="q"]').should(be.blank).type('qwerqwerfwqeferwfverdsvgsrtevsrtevsretv').press_enter()
-    assert browser.element('[id="search"]').should(have.no.text('Selene - User-oriented Web UI browser tests in Python'))
+    assert browser.element('[id="topstuff"]').should(have.text('По запросу qwerqwerfwqeferwfverdsvgsrtevsrtevsretv ничего не найдено. '))
